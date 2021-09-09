ed705f2d33e47c313cf016d14bd5c351644dc83d
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2021-09-10 00:22:23 +0700

    First Build App Simple Memo
    Package activity : 
    - EditActivity
    - MainActivity
    
    Package room: 
    - Note
    - NoteDao
    - NoteDB
    -

---
+++ app/src/main/java/com/course/crudsqlite/activity/EditActivity.kt
@@ -0,0 +1,38 @@
+package com.course.crudsqlite.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.course.crudsqlite.R
+import com.course.crudsqlite.room.Note
+import com.course.crudsqlite.room.NoteDB
+import kotlinx.android.synthetic.main.activity_edit.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class EditActivity : AppCompatActivity() {
+
+    val db by lazy { NoteDB(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+        setupListener()
+    }
+
+    fun setupListener(){
+        button_create.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.noteDao().addNote(
+                    Note(
+                        0,
+                        edit_title.text.toString(),
+                        edit_note.text.toString()
+                    )
+                )
+                finish()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/course/crudsqlite/room/NoteDao.kt
@@ -0,0 +1,19 @@
+package com.course.crudsqlite.room
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    fun addNote(note: Note)
+
+    @Update
+    fun updateNote(note: Note)
+
+    @Delete
+    fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM note")
+    fun getNotes(): List<Note>
+}
